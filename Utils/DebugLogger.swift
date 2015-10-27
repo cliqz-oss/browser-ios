bc728e83513c6feb6801d55c8feffab1039348a5
Mahmoud Adam <mahmoud@cliqz.com>
2015-10-27 13:30:06 +0100

    Added DebugLogger to enable logging messages with TimeStamp only during debugging

---
+++ Utils/DebugLogger.swift
@@ -0,0 +1,28 @@
+//
+//  DebugLogger.swift
+//  Client
+//
+//  Created by Mahmoud Adam on 10/27/15.
+//  Copyright (c) 2015 Cliqz. All rights reserved.
+//
+
+import UIKit
+
+class DebugLogger {
+    class func log<T>(@autoclosure object: () -> T) {
+        #if DEBUG
+            let value = object()
+            let stringRepresentation: String
+            
+            if let value = value as? CustomDebugStringConvertible {
+                stringRepresentation = value.debugDescription
+            } else if let value = value as? CustomStringConvertible {
+                stringRepresentation = value.description
+            } else {
+                fatalError("printLog only works for values that conform to CustomDebugStringConvertible or CustomStringConvertible")
+            }
+            
+            NSLog("[DEBUG] \(stringRepresentation)")
+        #endif
+    }
+}
