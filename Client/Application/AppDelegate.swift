cdd13179d81af569d7ee783088445fe1af896f17
Sahakyan <naira@cliqz.com>
2015-12-03 15:32:38 +0100

    Integrated last version of Extension and updated release configuration

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -121,7 +121,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
 		let bundlePath = NSBundle.mainBundle().bundlePath
-		let extensionPath = bundlePath + "/" + "mobile"
+		let extensionPath = bundlePath + "/" + "search"
 		
 		let httpServer = HttpServer()
 		httpServer["/extension/(.+)"] = HttpHandlers.directory(extensionPath)
