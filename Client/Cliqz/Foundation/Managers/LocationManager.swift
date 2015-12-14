41e5b3dcb841ba5366f47e407862d5b97336d7fd
Sahakyan <naira@cliqz.com>
2015-12-14 14:21:36 +0100

    Added LocationManager

---
+++ Client/Cliqz/Foundation/Managers/LocationManager.swift
@@ -0,0 +1,35 @@
+//
+//  LocationManager.swift
+//  Client
+//
+//  Created by Sahakyan on 8/28/15.
+//  Copyright (c) 2015 Mozilla. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+public class LocationManager: NSObject, CLLocationManagerDelegate {
+	
+	let manager = CLLocationManager()
+	var location: CLLocation?
+	
+	public static let sharedInstance: LocationManager = {
+		let m = LocationManager()
+		m.manager.delegate = m
+		m.manager.desiredAccuracy = 300
+		return m
+	}()
+	
+	public func startUpdateingLocation() {
+		self.manager.requestWhenInUseAuthorization()
+		self.manager.startUpdatingLocation()
+	}
+	
+	public func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		if locations.count > 0 {
+			self.location = locations[locations.count - 1]
+		}
+	}
+
+}
