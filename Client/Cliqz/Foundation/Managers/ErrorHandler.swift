66fd7ef63bfdc89f5e4ebe6f97bd684308ab190d
Mahmoud Adam <mahmoud@cliqz.com>
2016-09-09 11:28:02 +0200

    [IB-582] disable showing load error message

--- Client/Cliqz/Foundation/Managers/ErrorHandler.swift
+++ Client/Cliqz/Foundation/Managers/ErrorHandler.swift
@@ -19,11 +19,15 @@ class ErrorHandler {
 	internal class func handleError(errorCode: CliqzErrorCode, delegate: UIAlertViewDelegate, error: NSError) {
 		switch (errorCode) {
 		case .CliqzErrorCodeScriptsLoadingFailed:
+            // disable showing load error message
+            /*
 			let title = NSLocalizedString("Sorry", tableName: "Cliqz", comment: "Error message title")
 			let message = NSLocalizedString("Something went wrong, the script wasn't loaded.", tableName: "Cliqz", comment:"Error message when JS scripts for search can't be loaded")
 			let cancel = NSLocalizedString("Cancel", tableName: "Cliqz", comment: "Cancel")
 			let retry = NSLocalizedString("Retry", tableName: "Cliqz", comment: "Button label on Alert view ")
+            
 			showErrorMessage(title, message: message, cancelButtonTitle: cancel, otherButtonTitle: retry, delegate: delegate)
+            */
             
             let attirbutes = ["errorDescription": error.description]
             Answers.logCustomEventWithName("JavaScriptLoadError", customAttributes: attirbutes)
