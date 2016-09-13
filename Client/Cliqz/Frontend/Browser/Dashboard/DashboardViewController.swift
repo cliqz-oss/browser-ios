00c6c41898db93f5c9d3b3b7d1c097272bfadb8d
Sahakyan <naira@cliqz.com>
2016-09-13 17:04:07 +0200

    Change top sites strings

--- Client/Cliqz/Frontend/Browser/Dashboard/DashboardViewController.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/DashboardViewController.swift
@@ -84,6 +84,7 @@ class DashboardViewController: UIViewController, HistoryDelegate, FavoritesDeleg
 
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
+		AppDelegate.changeStatusBarColor(self.view.backgroundColor!)
 		self.navigationController?.navigationBarHidden = false
 		self.navigationController?.navigationBar.shadowImage = UIImage()
 		self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics:  .Default)
