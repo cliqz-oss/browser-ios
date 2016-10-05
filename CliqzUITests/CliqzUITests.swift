f4976925c5525c4ed34dae4dc36d9a201473075a
Mahmoud Adam <mahmoud@cliqz.com>
2016-10-05 17:47:33 +0200

    commented invalid tests methods (old History and settings)

--- CliqzUITests/CliqzUITests.swift
+++ CliqzUITests/CliqzUITests.swift
@@ -9,23 +9,6 @@
 import XCTest
 
 class CliqzUITests: KIFTestCase {
-
-    func testHistoryButton() {
-		// Use recording to get started writing UI tests.
-		tester.waitForViewWithAccessibilityLabel("HistoryButton")
-		tester.tapViewWithAccessibilityLabel("HistoryButton")
-		XCTAssertTrue(tester.viewExistsWithLabel("CloseHistoryButton"))
-		tester.tapViewWithAccessibilityLabel("CloseHistoryButton")
-	}
-
-	func testSettingsButton() {
-		tester.waitForViewWithAccessibilityLabel("HistoryButton")
-		tester.tapViewWithAccessibilityLabel("HistoryButton")
-//		XCTAssertTrue(tester.viewExistsWithLabel("OpenSettingsButton"))
-//		tester.tapViewWithAccessibilityLabel("OpenSettingsButton")
-//		XCTAssertTrue(tester.viewExistsWithLabel(NSLocalizedString("Done", comment: "Done button on left side of the Settings view controller title bar")))
-//		tester.tapViewWithAccessibilityLabel(NSLocalizedString("Done", comment: "Done button on left side of the Settings view controller title bar"))
-	}
 	
 	func testSearch() {
 		tester.waitForViewWithAccessibilityLabel(NSLocalizedString("Address and Search", comment: "Accessibility label for address and search field, both words (Address, Search) are therefore nouns."))
