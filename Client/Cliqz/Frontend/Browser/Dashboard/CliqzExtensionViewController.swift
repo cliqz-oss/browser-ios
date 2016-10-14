9ea1ed0c1a253e818ccef8adc28b1679da2509c3
Mahmoud Adam <mahmoud@cliqz.com>
2016-10-14 12:41:57 +0200

    [IB-595] Load history and favorites views immediately when opening the dashboard not on demand (not lazy)

--- Client/Cliqz/Frontend/Browser/Dashboard/CliqzExtensionViewController.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/CliqzExtensionViewController.swift
@@ -43,11 +43,18 @@ class CliqzExtensionViewController: UIViewController,  UIAlertViewDelegate {
         self.viewType = viewType
 		super.init(nibName: nil, bundle: nil)
 	}
-	
+    func loadExtensionWebView() {
+        if self.extensionWebView.URL == nil {
+            let url = NSURL(string:  self.mainRequestURL())
+            self.extensionWebView.loadRequest(NSURLRequest(URL: url!))
+        }
+    }
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 	override func viewWillLayoutSubviews() {
 		super.viewWillLayoutSubviews()
 		self.setupConstraints()
@@ -55,10 +62,7 @@ class CliqzExtensionViewController: UIViewController,  UIAlertViewDelegate {
 
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
-		if self.extensionWebView.URL == nil {
-			let url = NSURL(string:  self.mainRequestURL())
-			self.extensionWebView.loadRequest(NSURLRequest(URL: url!))
-		}
+        self.loadExtensionWebView()
 	}
 
 	override func viewDidAppear(animated: Bool) {
