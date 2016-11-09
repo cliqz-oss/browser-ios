05824252bb5d1a4422f8343b6d47970629be6462
Mahmoud Adam <mahmoud@cliqz.com>
2016-11-09 14:42:10 +0100

    Refactored YouTube downloader class and download the video only when user authorize the app to access Photos Library

--- Client/Cliqz/Foundation/Helpers/YoutubeVideoDownloader.swift
+++ Client/Cliqz/Foundation/Helpers/YoutubeVideoDownloader.swift
@@ -23,31 +23,47 @@ class YoutubeVideoDownloader {
 			videoUrl = videoUrl.stringByAddingPercentEncodingWithAllowedCharacters(.URLFragmentAllowedCharacterSet())!
 			let directoryURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
 			var localPath: NSURL = directoryURL.URLByAppendingPathComponent(NSDate().description)!
-			PHPhotoLibrary.requestAuthorization({ (authorizationStatus: PHAuthorizationStatus) -> Void in
-				Alamofire.download(.GET, videoUrl, destination: {  (temporaryURL, response) -> NSURL in
-					let pathComponent = NSDate().description + (response.suggestedFilename ?? "")
-					localPath = directoryURL.URLByAppendingPathComponent(pathComponent)!
-					return localPath
-				})
-				.response {
-					(request, response, _, error) in
-					PHPhotoLibrary.sharedPhotoLibrary().performChanges({
-						PHAssetChangeRequest.creationRequestForAssetFromVideoAtFileURL(localPath)}) { completed, error in
-							if completed {
-								// TODO: Postponed showing status info for the next release
-								TelemetryLogger.sharedInstance.logEvent(.YoutubeVideoDownloader("download", "is_success", "true"))
-								print("Video asset created")
-							} else {
-								TelemetryLogger.sharedInstance.logEvent(.YoutubeVideoDownloader("download", "is_success", "false"))
-								// TODO: Postponed showing status info for the next release
-								print("Download failed: \(error)")
-							}
-					}
-					print(response)
-					print("Error: \(error)")
-				}
+			
+            PHPhotoLibrary.requestAuthorization({ (authorizationStatus: PHAuthorizationStatus) -> Void in
+                
+                if authorizationStatus == .Authorized {
+                    //TODO: Show status info for the user
+                    print("[VedioDownloader] Download Started")
+                    
+                    Alamofire.download(.GET, videoUrl, destination: {  (temporaryURL, response) -> NSURL in
+                        let pathComponent = NSDate().description + (response.suggestedFilename ?? "")
+                        localPath = directoryURL.URLByAppendingPathComponent(pathComponent)!
+                        return localPath
+                    })
+                        .response {
+                            (request, response, _, error) in
+                            if error != nil {
+                                //TODO: Show status info for the user
+                                print("[VedioDownloader] Download failed: \(error)")
+                            } else {
+                                YoutubeVideoDownloader.saveVideoToPhotoLibrary(localPath)
+                            }
+                    }
+                }
+				
 			})
 		}
 	}
 	
+    private class func saveVideoToPhotoLibrary(localPath: NSURL) {
+        PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideoAtFileURL(localPath)}) { completed, error in
+                if completed {
+                    TelemetryLogger.sharedInstance.logEvent(.YoutubeVideoDownloader("download", "is_success", "true"))
+                    //TODO: Show status info for the user
+                    print("[VedioDownloader] Video asset created")
+                } else {
+                    TelemetryLogger.sharedInstance.logEvent(.YoutubeVideoDownloader("download", "is_success", "false"))
+                    //TODO: Show status info for the user
+                    print("[VedioDownloader] Could not save video file on Phone: \(error)")
+                }
+
+        }
+    }
+    
 }
