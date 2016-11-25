4ece8c3dad13b93787d1ccb4581ca6d4e7ec552b
Sahakyan <naira@cliqz.com>
2016-11-25 16:02:45 +0100

    Bug fix: [IB-585] - Tab overview - last tab cut off

--- Client/Cliqz/Frontend/Browser/Dashboard/DashboardViewController.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/DashboardViewController.swift
@@ -56,7 +56,7 @@ class DashboardViewController: UIViewController, HistoryDelegate, FavoritesDeleg
 		fatalError("init(coder:) has not been implemented")
 	}
 
-	override func viewDidLoad(	) {
+	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		panelSwitchContainerView = UIView()
