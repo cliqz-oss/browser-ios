a914a7946dc19e590036ec7414da5c32cae52144
Mahmoud Adam <mahmoud@cliqz.com>
2016-12-22 15:13:38 +0100

    [IB-616] fixed the issue of not sending context_menu telemetry signal when clicking new forget tab by making sure that the updating forget mode status will run after logging the signal not before

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -3459,9 +3459,9 @@ extension BrowserViewController: ContextMenuHelperDelegate {
                 
                 let openNewPrivateTabAction =  UIAlertAction(title: openNewPrivateTabTitle, style: UIAlertActionStyle.Default) { (action: UIAlertAction) in
                     self.scrollController.showToolbars(animated: !self.scrollController.toolbarsShowing, completion: { _ in
+                        TelemetryLogger.sharedInstance.logEvent(.ContextMenu("new_forget_tab", "link"))
                         self.preserveSearchState()
                         self.tabManager.addTab(NSURLRequest(URL: url), isPrivate: true)
-                        TelemetryLogger.sharedInstance.logEvent(.ContextMenu("new_forget_tab", "link"))
                     })
                 }
                 actionSheetController.addAction(openNewPrivateTabAction)
