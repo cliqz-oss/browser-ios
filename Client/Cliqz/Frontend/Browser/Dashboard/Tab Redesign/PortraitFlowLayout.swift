c385b6785c1ab66c2a89b2aae6246ecd49112635
Tim Palade <timotei@cliqz.com>
2017-04-27 10:39:09 +0200

    Tab Overview Redesign

---
+++ Client/Cliqz/Frontend/Browser/Dashboard/Tab Redesign/PortraitFlowLayout.swift
@@ -0,0 +1,82 @@
+//
+//  PortraitFlowLayout.swift
+//  TabsRedesign
+//
+//  Created by Tim Palade on 3/29/17.
+//  Copyright © 2017 Tim Palade. All rights reserved.
+//
+
+import UIKit
+
+class TabSwitcherLayoutAttributes: UICollectionViewLayoutAttributes {
+    
+    var displayTransform: CATransform3D = CATransform3DIdentity
+    
+    override func copyWithZone(zone: NSZone) -> AnyObject {
+        let copy = super.copyWithZone(zone) as! TabSwitcherLayoutAttributes
+        copy.displayTransform = displayTransform
+        return copy
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        guard let attr = object as? TabSwitcherLayoutAttributes else { return false }
+        return super.isEqual(object) && CATransform3DEqualToTransform(displayTransform, attr.displayTransform)
+    }
+    
+}
+
+class PortraitFlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+        self.minimumInteritemSpacing = 200.0
+        self.minimumLineSpacing = 0.0
+        self.scrollDirection = .Vertical
+        self.sectionInset = UIEdgeInsetsMake(30, 0, 0, 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override class func layoutAttributesClass() -> AnyClass {
+        return TabSwitcherLayoutAttributes.self
+    }
+    
+    override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        let attr = super.initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath)
+        attr?.zIndex = itemIndexPath.item
+        return attr
+    }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attrs = super.layoutAttributesForElementsInRect(rect) else {return nil}
+        
+        return attrs.map({ attr in
+            return pimpedAttribute(attr)
+        })
+    }
+    
+    func pimpedAttribute(attribute: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        
+        if let attr = attribute.copy() as? TabSwitcherLayoutAttributes {
+            
+            attr.zIndex = attr.indexPath.item
+            
+            var t: CATransform3D = CATransform3DIdentity
+            
+            if let count = self.collectionView?.numberOfItemsInSection(0) where count > 1 {
+                
+                t.m34 = -1.0 / (CGFloat(1000))
+                t = CATransform3DRotate(t, -CGFloat(Knobs.tiltAngle), 1, 0, 0)
+                //attr.transform = CGAffineTransformIdentity
+            }
+            
+            //t = CATransform3DScale(t, 0.95, 0.95, 1)
+            attr.displayTransform = t
+            
+            return attr
+        }
+        return attribute
+        
+    }
+}
--- Utils/Extensions/UIImageExtensions.swift
+++ Utils/Extensions/UIImageExtensions.swift
@@ -61,4 +61,29 @@ extension UIImage {
     public static func templateImageNamed(name: String) -> UIImage? {
         return UIImage(named: name)?.imageWithRenderingMode(.AlwaysTemplate)
     }
+    
+    public class func UIImageWith(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    public func getPixelColor(pos: CGPoint) -> UIColor {
+        
+        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage!)!)
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let a = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let r = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
 }
