c6175962ec3619679c496aad0f025d1d0a84dcb5
Mahmoud Adam <mahmoud@cliqz.com>
2017-05-03 14:39:41 +0200

    [IB-1182] tops_count should be from 1 to 8

--- Client/Cliqz/Frontend/Browser/FreshtabViewController.swift
+++ Client/Cliqz/Frontend/Browser/FreshtabViewController.swift
@@ -593,7 +593,7 @@ extension FreshtabViewController: TopSiteCellDelegate {
 extension FreshtabViewController {
     private func logTopsiteSignal(action: String, index: Int) {
      
-        let customData: [String: AnyObject] = ["topsite_count": topSites.count, "index": index]
+        let customData: [String: AnyObject] = ["topsite_count": getDisplayedTopsitesCount(), "index": index]
         self.logFreshTabSignal(action, target: "topsite", customData: customData)
     }
     
@@ -603,7 +603,7 @@ extension FreshtabViewController {
     }
     
     private func logTopsiteEditModeSignal() {
-        let customData: [String: AnyObject] = ["topsite_count": topSites.count, "delete_count": topSitesIndexesToRemove.count, "view": "topsite_edit_mode"]
+        let customData: [String: AnyObject] = ["topsite_count": getDisplayedTopsitesCount(), "delete_count": topSitesIndexesToRemove.count, "view": "topsite_edit_mode"]
         self.logFreshTabSignal("click", target: nil, customData: customData)
     }
     
@@ -615,7 +615,7 @@ extension FreshtabViewController {
     
     private func logShowSignal() {
         let loadDuration = Int(NSDate.getCurrentMillis() - startTime)
-        var customData: [String: AnyObject] = ["topsite_count": topSites.count, "load_duration": loadDuration]
+        var customData: [String: AnyObject] = ["topsite_count": getDisplayedTopsitesCount(), "load_duration": loadDuration]
         if isLoadCompleted {
             customData["is_complete"] = true
             let breakingNews = news.filter() {
@@ -646,5 +646,11 @@ extension FreshtabViewController {
     private func logFreshTabSignal(action: String, target: String?, customData: [String: AnyObject]?) {
         TelemetryLogger.sharedInstance.logEvent(.FreshTab(action, target, customData))
     }
+    private func getDisplayedTopsitesCount() -> Int {
+        let topSitesCount = topSites.count
+        return (topSitesCount > 8) ? 8 : topSitesCount
+
+    }
+
     
 }
