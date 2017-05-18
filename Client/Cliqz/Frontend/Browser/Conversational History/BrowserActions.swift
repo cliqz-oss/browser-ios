bcc63502255e97894ce12739dc00a7f7e6eda9f9
Sahakyan <naira@cliqz.com>
2017-05-18 15:32:38 +0200

    openLink

--- Client/Cliqz/Frontend/Browser/Conversational History/BrowserActions.swift
+++ Client/Cliqz/Frontend/Browser/Conversational History/BrowserActions.swift
@@ -14,6 +14,9 @@ public class BrowserActions: NSObject {
 	@objc(openLink:)
 	public func openLink(url: NSString) {
 		print("Hello React")
+		if let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate, u = NSURL(string:url as String) {
+			appDelegate.openUrlInWebView(u)
+		}
 	}
 	
 	@objc(queryCliqz:)
