9f30ce134d37f63f26815ba7fbb90365484525e8
Tim Palade <timotei@cliqz.com>
2017-07-14 14:52:38 +0200

    Open in Cliqz

---
+++ Open In Cliqz/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  Open In Cliqz
+//
+//  Created by Tim Palade on 7/13/17.
+//  Copyright © 2017 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
