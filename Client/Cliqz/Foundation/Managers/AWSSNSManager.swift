1d58c6494d4e48d9dd2ccb1f0d17cd494573ce51
Mahmoud Adam <mahmoud@cliqz.com>
2017-08-04 17:28:35 +0200

    [IB-1410] Implemented soccer subscriptions

--- Client/Cliqz/Foundation/Managers/AWSSNSManager.swift
+++ Client/Cliqz/Foundation/Managers/AWSSNSManager.swift
@@ -9,14 +9,30 @@
 import Foundation
 
 class AWSSNSManager {
-	
-	static let tokenKey = "Token"
-	
-	static let cognitoIdentityPoolID = "us-east-1:81faca92-4d48-437c-ad68-e28ca03411fe"
-//	static let SNSAplicationArn = "arn:aws:sns:us-east-1:141047255820:app/APNS_SANDBOX/Cliqz_Beta_for_iOS"
-	static let SNSAplicationArn = "arn:aws:sns:us-east-1:141047255820:app/APNS/Cliqz_Beta_Production_for_iOS"
-	static let cognitoRegionID = AWSRegionType.usEast1
-	
+    //MARK: - Internal Variables
+    static let cognitoRegionID = AWSRegionType.usEast1
+    private static let cognitoIdentityPoolID = "us-east-1:81faca92-4d48-437c-ad68-e28ca03411fe"
+    
+#if BETA
+    // Note that we only subscribe for one topic and all notifications are sent to all users as a silent notification then will be filtered on the client side
+    private static let notificationTopic = "arn:aws:sns:us-east-1:141047255820:mobile_news_staging"
+    
+    // Debug
+//	private static let SNSAplicationArn = "arn:aws:sns:us-east-1:141047255820:app/APNS_SANDBOX/Cliqz_Beta_for_iOS"
+    // TestFlight
+    private static let SNSAplicationArn = "arn:aws:sns:us-east-1:141047255820:app/APNS/Cliqz_Beta_Production_for_iOS"
+
+#else
+    // Note that we only subscribe for one topic and all notifications are sent to all users as a silent notification then will be filtered on the client side
+    private static let notificationTopic = "arn:aws:sns:us-east-1:141047255820:mobile_news"
+    
+    // Release
+    private static let SNSAplicationArn = "arn:aws:sns:us-east-1:141047255820:app/APNS/CLIQZ_Browser_for_iOS"
+#endif
+    
+    private static let tokenKey = "DeviceToken"
+    
+    //MARK: - Public APIs
 	class func configureCongnitoPool() {
 		let credentialsProvider = AWSCognitoCredentialsProvider(
 			regionType: cognitoRegionID,
@@ -27,7 +43,7 @@ class AWSSNSManager {
 		AWSServiceManager.default().defaultServiceConfiguration = defaultServiceConfiguration
 	}
 	
-	class func createPlatformEndpoint(_ deviceToken: String) {
+	class func createPlatformEndpoint(withDeviceToken deviceToken: String, completionHandler: @escaping (Bool) -> Void) {
 		if let oldToken = (LocalDataStore.objectForKey(tokenKey) as? String), oldToken == deviceToken {
 			return
 		}
@@ -38,30 +54,30 @@ class AWSSNSManager {
 		request?.platformApplicationArn = SNSAplicationArn
 		sns.createPlatformEndpoint(request!).continue(with: AWSExecutor.mainThread(), with: { (task: AWSTask!) -> AnyObject! in
 			if task.error != nil {
-				debugPrint("Error Creating Endpoint: \(task.error)")
 				LocalDataStore.removeObjectForKey(tokenKey)
-			} else {
-				let createEndpointResponse = task.result as! AWSSNSCreateEndpointResponse
-				if let endpointArn = createEndpointResponse.endpointArn {
-					subscriptForTopic(endpointArn)
-					LocalDataStore.setObject(deviceToken, forKey: tokenKey)
-				}
-			}
+			} else if let createEndpointResponse = task.result,
+                let endpointArn = createEndpointResponse.endpointArn {
+                LocalDataStore.setObject(deviceToken, forKey: tokenKey)
+                subscriptForNotification(endpointArn, completionHandler: completionHandler)
+                return nil
+            }
+            completionHandler(false)
 			return nil
 		})
 	}
 	
-	class func subscriptForTopic(_ endpointArn: String) {
+	class func subscriptForNotification(_ endpointArn: String, completionHandler: @escaping (Bool) -> Void) {
 		let sns = AWSSNS.default()
 		let input = AWSSNSSubscribeInput()
-		input?.topicArn = "arn:aws:sns:us-east-1:141047255820:mobile_news"
+		input?.topicArn = notificationTopic
 		input?.endpoint = endpointArn
 		input?.protocols = "application"
 		sns.subscribe(input!, completionHandler: { (response, error) -> Void in
 			if error != nil {
-				debugPrint("Error subscribing for a topic: \(error)")
+				debugPrint("Error subscribing for a topic: \(error.debugDescription)")
+                completionHandler(false)
 			} else {
-				debugPrint("Subscribtion succeeded")
+				completionHandler(true)
 			}
 		})
 	}
