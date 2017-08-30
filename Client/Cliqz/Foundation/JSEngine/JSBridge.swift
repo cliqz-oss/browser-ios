1702981016e69b74f1d7dd48563e0a1b092def9b
Sam Macbeth <sam@cliqz.com>
2017-08-30 16:19:13 +0200

    Implement CliqzEvent forwarding to native.

--- Client/Cliqz/Foundation/JSEngine/JSBridge.swift
+++ Client/Cliqz/Foundation/JSEngine/JSBridge.swift
@@ -47,6 +47,15 @@ open class JSBridge : RCTEventEmitter {
         return ["callAction", "publishEvent"]
     }
     
+    override open func constantsToExport() -> [String : Any]! {
+        return ["events": ["openUrl", "mobile-pairing:openTab",
+                           "mobile-pairing:downloadVideo",
+                           "mobile-pairing:pushPairingData",
+                           "mobile-pairing:notifyPairingError",
+                           "mobile-pairing:notifyPairingSuccess"]
+        ]
+    }
+    
     fileprivate func nextActionId() -> NSInteger {
         var nextId : NSInteger = 0
         lockDispatchQueue.sync {
@@ -181,7 +190,7 @@ open class JSBridge : RCTEventEmitter {
     
     
     @objc(pushEvent:data:)
-    func pushEvent(eventId: NSString, data: NSDictionary) {
+    func pushEvent(eventId: NSString, data: NSArray) {
         DispatchQueue.main.async() {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: eventId as String), object: data, userInfo: nil)
         }
