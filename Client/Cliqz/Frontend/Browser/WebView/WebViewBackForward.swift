42805a1ba07efbc642883d7dec722b4221762b8c
Tim Palade <timotei@cliqz.com>
2017-09-27 09:14:28 +0200

    Added some things for the back forward navigation

--- Client/Cliqz/Frontend/Browser/WebView/WebViewBackForward.swift
+++ Client/Cliqz/Frontend/Browser/WebView/WebViewBackForward.swift
@@ -99,10 +99,10 @@ class WebViewBackForwardList {
             }
             i += 1
         }
-        if  backForwardList.count > 0 {
-            let lastUrlStr = backForwardList[backForwardList.count - 1].url.absoluteString
-            StateManager.shared.handleAction(action: Action(data: ["url" : lastUrlStr], type: .visitAddedInDB))
-        }
+//        if  backForwardList.count > 0 {
+//            let lastUrlStr = backForwardList[backForwardList.count - 1].url.absoluteString
+//            StateManager.shared.handleAction(action: Action(data: ["url" : lastUrlStr], type: .visitAddedInDB))
+//        }
         
         if !foundCurrent {
             currentIndex = 0
