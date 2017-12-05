24b6d5319db1a5e81f7816060c15009ad2b62a27
Sahakyan <naira@cliqz.com>
2017-12-05 17:25:54 +0100

    API fixes

--- Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzViewController.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzViewController.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 class OffrzViewController: UIViewController {
-	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		OffrzDataService.shared.getMyOffrz { (ofrrz, error) in
+			print("Hello")
+		}
+	}
 }
