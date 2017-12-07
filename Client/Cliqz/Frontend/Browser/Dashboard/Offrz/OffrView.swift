a2c59e192141e4e1f1c57b035106e15f402c4f93
Sahakyan <naira@cliqz.com>
2017-12-07 13:16:27 +0100

    Update Dashboard view with Offrz

---
+++ Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrView.swift
@@ -0,0 +1,13 @@
+//
+//  OffrView.swift
+//  Client
+//
+//  Created by Sahakyan on 12/7/17.
+//  Copyright © 2017 Mozilla. All rights reserved.
+//
+
+import Foundation
+
+class OffrView: UIView {
+	
+}
