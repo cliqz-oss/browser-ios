30931945429c563a181365f00e20ac96f181ea17
Sahakyan <naira@cliqz.com>
2017-12-13 16:17:08 +0100

    Added Offr Expiration/Updating logic

--- Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzDataSource.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzDataSource.swift
@@ -10,31 +10,85 @@ import Foundation
 
 class OffrzDataSource {
 	
-	private var currentOffr: Offrz?
-	var lastOffrz = [Offrz]()
+	private var currentOffr: Offr?
+	private static let LastSeenOffrID = "LastSeenOffrID"
+
+	var myOffrz = [Offr]()
 
 	init() {
+		self.updateMyOffrzList()
+	}
+
+	func getCurrentOffr() -> Offr? {
+		if let o = self.currentOffr,
+			self.isExpiredOffr(o) {
+			self.currentOffr = nil
+			// TODO: Impl notify on currentOffr change....
+			self.updateMyOffrzList()
+		}
+		return self.currentOffr
+	}
+
+	func shouldShowOffrz() -> Bool {
+		return SettingsPrefs.shared.getRegionPref() == "DE"
+	}
+	
+	func hasUnseenOffrz() -> Bool {
+		return !(self.currentOffr?.isSeen ?? true)
+	}
+
+	func markCurrentOffrSeen() {
+		self.currentOffr?.isSeen = true
+		LocalDataStore.setObject(self.currentOffr?.uid, forKey: OffrzDataSource.LastSeenOffrID)
+	}
+
+	private func updateMyOffrzList() {
 		OffrzDataService.shared.getMyOffrz { (offrz, error) in
 			if error == nil && offrz.count > 0 {
-				self.currentOffr = offrz.first
+				self.myOffrz = offrz
+				if let o = self.getLastSeenOffr() {
+					self.currentOffr = o
+					self.currentOffr?.isSeen = true
+				} else {
+				LocalDataStore.removeObjectForKey(OffrzDataSource.LastSeenOffrID)
+					self.currentOffr = self.getNotExpiredOffr()
+				}
 			}
 		}
 	}
 
-	func getCurrentOffr() -> Offrz? {
-		return self.currentOffr
+	private func getNotExpiredOffr() -> Offr? {
+		for o in self.myOffrz {
+			if !self.isExpiredOffr(o) {
+				return o
+			}
+		}
+		return nil
 	}
 
-	func hasUnreadOffrz() -> Bool {
-		// TODO: ....
-		return true
+	private func getLastSeenOffr() -> Offr? {
+		let offrID = self.getLastSeenOffrID()
+		for o in self.myOffrz {
+			if o.uid == offrID {
+				if !self.isExpiredOffr(o) {
+					return o
+				}
+			}
+		}
+		return nil
 	}
-    
-    func shouldShowOffrz() -> Bool {
-        return SettingsPrefs.shared.getRegionPref() == "DE" 
-    }
 
-	func markRead() {
-		
+	private func getLastSeenOffrID() -> String? {
+		return LocalDataStore.objectForKey(OffrzDataSource.LastSeenOffrID) as? String
 	}
+
+	private func isExpiredOffr(_ offr: Offr) -> Bool {
+		let now =  Date()
+		if let start = offr.startDate,
+			let end = offr.endDate {
+			return start > now || end < now
+		}
+		return false
+	}
+
 }
