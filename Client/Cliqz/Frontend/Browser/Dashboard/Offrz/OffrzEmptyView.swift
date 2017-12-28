2e124d933a2d17e4a745bffef8925027d96cf677
Sahakyan <naira@cliqz.com>
2017-12-28 15:20:26 +0100

    Refactoring of Offrz onboarding view

---
+++ Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzEmptyView.swift
@@ -0,0 +1,13 @@
+//
+//  OffrzEmptyView.swift
+//  Client
+//
+//  Created by Sahakyan on 12/27/17.
+//  Copyright © 2017 Mozilla. All rights reserved.
+//
+
+import Foundation
+
+class OffrzEmptyView: UIView {
+	
+}
