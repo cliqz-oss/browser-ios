60a56e77fac02d9c5621cfc8d825c47b1dc44b5f
Sahakyan <naira@cliqz.com>
2017-12-28 16:41:25 +0100

    Refactoring of Offrz empty view

--- Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzEmptyView.swift
+++ Client/Cliqz/Frontend/Browser/Dashboard/Offrz/OffrzEmptyView.swift
@@ -9,5 +9,53 @@
 import Foundation
 
 class OffrzEmptyView: UIView {
+	private let offrzPresentImageView = UIImageView(image: UIImage(named: "offrz_present"))
+	private let emptyTextLabel = UILabel()
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.setup()
+	}
 	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		self.setup()
+	}
+	
+	init() {
+		super.init(frame: CGRect.zero)
+		self.setup()
+	}
+
+	override func updateConstraints() {
+		super.updateConstraints()
+		self.layoutComponents()
+	}
+
+	private func setup() {
+		self.setupComponents()
+		self.setStyles()
+		self.layoutComponents()
+	}
+
+	private func setupComponents() {
+		self.addSubview(emptyTextLabel)
+		self.addSubview(offrzPresentImageView)
+		emptyTextLabel.text = NSLocalizedString("MyOffrz Empty Description", tableName: "Cliqz", comment: "[MyOffrz] No offers label")
+	}
+
+	private func setStyles() {
+		emptyTextLabel.textColor = UIColor.gray
+	}
+
+	private func layoutComponents() {
+		offrzPresentImageView.snp.remakeConstraints({ (make) in
+			make.centerX.equalTo(self)
+			make.centerY.equalTo(self).dividedBy(2)
+		})
+		emptyTextLabel.snp.remakeConstraints({ (make) in
+			make.centerX.equalTo(self)
+			make.top.equalTo(offrzPresentImageView.snp.bottom).offset(10)
+		})
+	}
 }
